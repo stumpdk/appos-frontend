0fcae527a3db5c924f5a5bd202cd1c63f38b1c9b
Bo Henriksen <stumpdenmark@hotmail.com>
2023-05-01 22:56:13 +0200

    Add customer service

---
+++ src/app/customer.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Customer } from './shared/models/customer';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  private apiUrl : string;
+
+  constructor(private http: HttpClient) {
+    this.apiUrl = environment.apiUrl + "/Customers";
+  }
+
+  public getCustomers() : Observable<Customer[]> {
+    return this.http.get<Customer[]>(this.apiUrl);
+  }
+
+  public GetCustomerById(id: number) : Observable<Customer> {
+    return this.http.get<Customer>(this.apiUrl + '/' + id);
+  }
+
+  public AddCustomer(customer: Customer) : Observable<Customer> {
+    return this.http.post<Customer>(this.apiUrl, customer);
+  }
+
+  public UpdateCustomer(customer: Customer) : Observable<Customer> {
+    return this.http.put<Customer>(this.apiUrl, customer);
+  }
+
+}
